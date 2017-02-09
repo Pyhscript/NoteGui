b902abb3c1876930c4760fdaa12d6d68919569ef
syncthetic <git@x509.33mail.com>
2017-02-09 13:18:57 -0600

    Getting dirty with the basics

---
+++ README.md
@@ -0,0 +1,24 @@
+# NoteGui
+
+**An easy to use multi-platform application for notes**
+
+**Use this app along with the [Electron API Demos](http://electron.atom.io/#get-started) app for API code examples to help you get started.**
+
+## To Use
+
+To clone and run this repository you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer. From your command line:
+
+```bash
+# Clone this repository
+git clone https://github.com/syncthetic/NoteGui
+# Go into the repository
+cd NoteGui
+# Install dependencies
+npm install
+# Run the app
+npm start
+```
+
+**NoteGui uses a remote server with an integrated API system**
+
+You can find it [here](#)!
---
+++ api.js
@@ -0,0 +1,65 @@
+function APISync() {
+  this.token = '',
+  this.server = 'http://mshredsuxx.ml:1337',
+
+  this.login = function(user, pass, parent=this) {
+    $.ajax({
+      url:      this.server + '/login',
+      method:   "POST",
+      dataType: "JSON",
+      data: {
+        username: user,
+        password: pass,
+      }
+    }).done(function(msg) {
+      if(msg.ec === 0) {
+        $('#api-call').html('API Returned: ' + msg.ec + ' #=> Successful');
+        $('#token').html('Token: ' + msg.token);
+        parent.token = msg.token;
+      }
+      else {
+        $('#api-call').html('API Returned: ' + msg.ec + ' #=> Failed');
+      }
+    });
+  } // end login(user, pass);
+
+  this.register = function(user, pass, pass2, group, parent=this) {
+    $.ajax({
+      url: parent.server + '/userprofile/create',
+      method: "POST",
+      dataType: "JSON",
+      data: {
+        username:   user,
+        password:   pass,
+        password2:  pass2,
+        groups:     group,
+        BITToken:   parent.token,
+      }
+    }).done(function(msg) {
+      if (msg.ec === 0) {
+        $('#api-call').html('Registration Returned: ' + msg.ec + ' #=> Registration Successful');
+      }
+      else {
+        $('#api-call').html('Registration Returned: ' + msg.ec + ' #=> Registration Failed.');
+      }
+    });
+  } // end register(user, pass, pass2, group)
+
+  this.logout = function(parent=this) {
+    $.ajax({
+      url: parent.server + '/logout',
+      method: "GET",
+      dataType: "JSON",
+      data: {
+        BITToken: parent.token,
+      }
+    }).done(function(msg) {
+      if(msg.ec === 0) {
+        $('#api-call').html('Logout Returned: ' + msg.ec + ' #=> Logout Successful');
+      } else {
+        $('#api-call').html('Logout Returned: ' + msg.ec + ' #=> Logout Failed');
+      }
+    });
+  } // end logout();
+
+} // end APISync();
